a019d17a2bbeb51a9be582815b2599774ef38c28
alireza <alireza.jvh98@gmail.com>
2021-09-24 09:41:55 +0330

    complete DatasetLoader class

--- dataloader/dataset_loader.py
+++ dataloader/dataset_loader.py
@@ -1,42 +1,35 @@
 # from torchvision import datasets, transforms
 # from base import BaseDataLoader
-from typing import List, Tuple
-from typing_extensions import ParamSpec
+from typing import Any, List, Tuple
+from torch.functional import Tensor
 from torch.utils.data import Dataset
 from utils.types import LearningPhase 
 import os.path as osp
 import os
-
-
-# class MnistDataLoader(BaseDataLoader):
-#     """
-#     MNIST data loading demo using BaseDataLoader
-#     """
-#     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-#         trsfm = transforms.Compose([
-#             transforms.ToTensor(),
-#             transforms.Normalize((0.1307,), (0.3081,))
-#         ])
-#         self.data_dir = data_dir
-#         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-#         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
-
-
+from PIL import Image
+from torchvision import transforms
+import numpy as np
 
 
 class DatasetLoader(Dataset):
 
     def __init__(self, setname: LearningPhase, dataset_dir: str, train_augment: bool = False):
-        
         PATH, label_list = self.__set_path(setname, dataset_dir)
-        print('here')
-        print(PATH)
-        print('label')
-        print(label_list)
+        data, label = [], []
+        folders_path = [osp.join(PATH, label_item) for label_item in label_list if os.path.isdir(osp.join(PATH, label_item))]
+        
+        for idx, folder_path in enumerate(folders_path):
+            image_list_path = os.listdir(folder_path)
+            for image_path in image_list_path:
+                data.append(osp.join(folder_path, image_path))
+                label.append(idx)
+        
+        self.data, self.label = data, label
+        self.num_class = len(set(label))
+        self.transform = self.__do_transform(train_augment)        
 
-    
+
     def __set_path(self, setname: LearningPhase, dataset_dir: str) -> Tuple[str, List[str]]:
-
         if setname == LearningPhase.TRAIN:
             PATH = osp.join(dataset_dir, 'train')
         elif setname == LearningPhase.TEST:
@@ -49,5 +42,34 @@ class DatasetLoader(Dataset):
 
         return PATH, label_list
 
+    def __do_transform(self, train_augment: bool) -> transforms.Compose:
+        image_size = 80
+        if train_augment:
+            transform = transforms.Compose([
+                transforms.Resize(92),
+                transforms.RandomResizedCrop(88),
+                transforms.CenterCrop(image_size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(np.array([x / 255.0 for x in [125.3, 123.0, 113.9]]),
+                                     np.array([x / 255.0 for x in [63.0, 62.1, 66.7]]))
+            ])
+        else:
+            transform = transforms.Compose([
+                transforms.Resize(92),
+                transforms.CenterCrop(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(np.array([x / 255.0 for x in [125.3, 123.0, 113.9]]),
+                                     np.array([x / 255.0 for x in [63.0, 62.1, 66.7]]))
+            ]) 
+        return transform
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, idx) -> Tuple[Any, int]:
+        path, label = self.data[idx], self.label[idx]
+        image = Image.open(path).convert('RGB')
+        return image, label
 
         
--- playground.py
+++ playground.py
@@ -2,3 +2,6 @@ from dataloader import DatasetLoader
 from utils.types import LearningPhase
 
 dataset = DatasetLoader(LearningPhase.TRAIN, './data/mini')
+
+for i in dataset:
+    print(i)
